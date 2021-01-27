c7859ff2fcd11bf03a25c6dd4fb6b41ee574f5c3
Enrique Ortiz <enriquechivas_@hotmail.com>
2021-01-27 00:03:25 -0700

    prueba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,15 +7,25 @@ export const environment = {
 };
 
 
-export const firebaseConfig = {
+/*export const firebaseConfig = {
   apiKey: "AIzaSyD6cXoSxbAzox2DuJERj6ljRpDyTnmcaqA",
   authDomain: "chatonline-e455a.firebaseapp.com",
   databaseURL: "https://chatonline-e455a.firebaseio.com",
   projectId: "chatonline-e455a",
   storageBucket: "chatonline-e455a.appspot.com",
   messagingSenderId: "372500599994"
+};*/
+export const environment = {
+  production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyDsaCUSMdBwWEHPB1Zg_v65Fhxmkpcm58U",
+    authDomain: "healthcare-c0622.firebaseapp.com",
+    projectId: "healthcare-c0622",
+    storageBucket: "healthcare-c0622.appspot.com",
+    messagingSenderId: "122387139883",
+    appId: "1:122387139883:web:2df22ed6cbdbb6df6a496e"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
